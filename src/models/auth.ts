eefe39450b480cc06d274c81337a1d1cc9220d15
fadyrsaweres <fady.ragaa48@gmail.com>
2025-12-21 15:38:53 +0200

    feat: add interfaces for authentication input, response, and user registration models.

--- src/models/auth.ts
+++ src/models/auth.ts
@@ -25,6 +25,57 @@ export interface RegisterInput {
     acceptTerms?: boolean,
 }
 
+export interface UserAuthority {
+    authority: string;
+}
+
+export interface RegisterResponse {
+    id: number;
+    createdAt: string;
+    updatedAt: string | null;
+    createdBy: string | null;
+    updatedBy: string | null;
+
+    fullName: string;
+    email: string;
+    passwordHash: string;
+    password: string;
+
+    username: string;
+    userType: "importer" | "exporter" | string;
+    phone: string;
+
+    active: boolean;
+    enabled: boolean;
+    isLoggedOut: boolean;
+
+    countryName: string | null;
+    companyName: string | null;
+    address: string | null;
+
+    comRegister: string | null;
+    taxNumber: string | null;
+    accountNumber: string | null;
+    activityType: string | null;
+    commissionerName: string | null;
+    statNumber: string | null;
+    companyLicense: string | null;
+    attachment: string | null;
+
+    failedLoginCount: number;
+    lastLoginDate: string | null;
+    userPreferredLanguage: string | null;
+
+    registrationStatus: "PENDING" | "APPROVED" | "REJECTED" | string;
+
+    credentialsNonExpired: boolean;
+    accountNonExpired: boolean;
+    accountNonLocked: boolean;
+
+    authorities: UserAuthority[];
+}
+
+
 
 
 export interface AuthResponse {
